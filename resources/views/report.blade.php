b96ee6f9f7684c4da957a33cae54b44a97c3a1dc
Ezekiel <iamwebwiz@gmail.com>
2019-02-06 16:10:49 +0100

    update report mail view

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -6,11 +6,29 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Reportr</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
 <body>
-    <h3 class="text-center">{{ $reportTitle }}</h3>
+    <h3 class="text-center">{!! $data['reportTitle'] !!}</h3>
 
-    <script src="{{ asset('js/app.js') }}"></script>
+    <h4>Employee Name</h4>
+    <p>{!! $data['employeeName'] !!}</p>
+
+    <h4>Date</h4>
+    <p>{!! $data['reportDate'] !!}</p>
+
+    <h4>Duration</h4>
+    <p>{!! $data['duration'] !!}</p>
+
+
+    <h4>Tasks</h4>
+    <ul class="list-group">
+        @foreach ($data['tasks'] as $task)
+            <li class="list-group-item">{!! $task['content'] !!}</li>
+        @endforeach
+    </ul>
+
+    <h4>Comments</h4>
+    <p>{!! $data['comments'] !!}</p>
 </body>
 </html>
