cc393e01077055069c0ab6a5dd4bfef6b19b17a9
Ezekiel <iamwebwiz@gmail.com>
2019-02-10 18:05:15 +0100

    update Report model

--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    protected $fillable = [
+        'company_name', 'report_title', 'employee_name', 'report_date',
+        'duration', 'tasks', 'comments'
+    ]
 }
