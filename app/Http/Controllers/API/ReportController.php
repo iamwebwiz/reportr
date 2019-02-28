e08966c66f124cf13a836664fa50803990e255a9
Ezekiel <iamwebwiz@gmail.com>
2019-02-28 13:16:49 +0100

    update report controller

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -21,40 +21,33 @@ class ReportController extends Controller
     public function sendReport(Request $request)
     {
 
-        if($request->isMethod('post')){
-            $report = new Report;
+        $report = new Report();
 
-            $report->company_name = $request->companyName;
-            $report->report_title = $request->reportTitle;
-            $report->employee_name = $request->employeeName;
-            $report->report_date = $request->reportDate;
-            $report->duration = $request->duration;
-            $report->tasks = json_encode($request->tasks);
-            $report->comments = $request->comments;
-            $report->save();
+        $report->company_name = $request->companyName;
+        $report->report_title = $request->reportTitle;
+        $report->employee_name = $request->employeeName;
+        $report->report_date = $request->reportDate;
+        $report->duration = $request->duration;
+        $report->tasks = json_encode($request->tasks);
+        $report->comments = $request->comments;
+        $report->save();
 
-            $report_id = $report->id;
-            $report = Report::where('id', $report_id)->first();
+        $report_id = $report->id;
 
-            $path = 'storage/pdfs/' . $report_id . '.pdf';
-            $pdf = PDF::loadView('pdfView', compact('report'));
-            $pdf->save($path);
+        $path = 'storage/reports/' . $report_id . '.pdf';
+        $pdf = PDF::loadView('pdfView', compact('report'));
+        $pdf->save($path);
 
-            $data = array (
-                'reportId' => $report_id,
-                'companyName' => $report->company_name,
-                'name' => $report->employee_name,
-                'reportDate' => $report->report_date
-            );
+        $data = array (
+            'reportId' => $report_id,
+            'companyName' => $report->company_name,
+            'name' => $report->employee_name,
+            'reportDate' => $report->report_date
+        );
 
-            Mail::to('itzchristar@gmail.com')->send(new ReportMail($data));
+        Mail::to('iamwebwiz@gmail.com')->send(new ReportMail($data));
 
-            return 'Email was sent';
+        return response()->json('Report has been sent', 201);
 
-        } else {
-            DB::rollback();
-        }
-       
-            
     }
 }
