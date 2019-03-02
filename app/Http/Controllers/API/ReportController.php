43ca66a9a1777d71e113e3b66a809901e0f24349
Ezekiel <iamwebwiz@gmail.com>
2019-03-02 02:36:36 +0100

    update Report controller

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -30,22 +30,18 @@ class ReportController extends Controller
         $report->duration = $request->duration;
         $report->tasks = json_encode($request->tasks);
         $report->comments = $request->comments;
+        $report->recipient = $request->recipient;
+        $report->sender_email = $request->senderEmail;
+        $report->sender_name = $request->senderName;
         $report->save();
 
-        $report_id = $report->id;
+        $path = 'storage/reports/' . $report['id'] . '.pdf';
+        $tasks = json_decode($report['tasks'], true);
 
-        $path = 'storage/reports/' . $report_id . '.pdf';
-        $pdf = PDF::loadView('pdfView', compact('report'));
+        $pdf = PDF::loadView('pdfView', compact('report', 'tasks'));
         $pdf->save($path);
 
-        $data = array (
-            'reportId' => $report_id,
-            'companyName' => $report->company_name,
-            'name' => $report->employee_name,
-            'reportDate' => $report->report_date
-        );
-
-        Mail::to('iamwebwiz@gmail.com')->send(new ReportMail($data));
+        Mail::to($report['recipient'])->send(new ReportMail($report));
 
         return response()->json('Report has been sent', 201);
 
