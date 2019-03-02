2088289e0d22604367dceec54b7ebcbac0ad48c2
Ezekiel <iamwebwiz@gmail.com>
2019-03-02 02:37:21 +0100

    update pdf view

--- resources/views/pdfView.blade.php
+++ resources/views/pdfView.blade.php
@@ -2,51 +2,39 @@
 <html>
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pdf download/stream app</title>
-
-    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
-    </head>
+  </head>
   <body>
 
-    <table class="table">
-      <thead>
-      <tr>
-        <th>Id</th>
-        <th>Company Name</th>
-        <th>Report Title</th>
-        <th>Employee Name</th>
-        <th>Report Date</th>
-        <th>Duration</th>
-        <th>Tasks</th>
-        <th>Comment</th>
-      </tr>
-    </thead>
-    @php
-    $tasks = json_decode($report->tasks);
-    @endphp
-
-    <tbody>
-      <tr>
-        <td>{{$report->id}}</td>
-        <td>{{$report->company_name}}</td>
-        <td> {{$report->report_title}}</td>
-        <td>{{$report->employee_name}}</td>
-        <td>{{$report->report_date}}</td>
-        <td>{{$report->duration}}</td>
-
-        <td>
-            @php
-                foreach ($tasks as $task) {
-                    echo '* '.$task->content; echo '<br>';
-                }
-            @endphp
-        </td>
-
-
-        <td>{{$report->comments}}</td>
-      </tr>
-    </tbody>
-    </table>
+    <div class="container">
+      <h4><strong>COMPANY NAME</strong></h4>
+      <p>{{ $report['company_name'] }}</p>
+      <br>
+      <h4><strong>REPORT TITLE</strong></h4>
+      <p>{{ $report['report_title'] }}</p>
+      <br>
+      <h4><strong>DATE</strong></h4>
+      <p>{{ date('d/m/Y', strtotime($report['report_date'])) }}</p>
+      <br>
+      <h4><strong>EMPLOYEE NAME</strong></h4>
+      <p>{{ $report['employee_name'] }}</p>
+      <br>
+      <h4><strong>TASK(S) COMPLETED</strong></h4>
+      <p>
+        <ul class="list-group">
+          @foreach ($tasks as $task)
+            <li class="list-group-item">{{ $task['content'] }}</li>
+          @endforeach
+        </ul>
+      </p>
+      <br>
+      <h4><strong>DURATION</strong></h4>
+      <p>{{ $report['duration'] }}</p>
+      <br>
+      <h4><strong>COMMENTS</strong></h4>
+      <p>{{ $report['comments'] }}</p>
+    </div>
 
   </body>
   </html>
